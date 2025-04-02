23fa4b7ab6849638d015179949e86922a3e23a81
abijith pc <abhijithchandrasenan007@gmail.com>
2025-04-02 10:23:42 +0530

    firebase

---
+++ lib/features/Splash/Presentation/splashscreen1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Splashscreen1 extends StatelessWidget {
+  const Splashscreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Image.asset('Assets/Logo/Untitled design.png'),),);
+  }
+}
