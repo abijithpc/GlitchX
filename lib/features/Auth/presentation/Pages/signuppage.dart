c59df842f4c4568f467f0dbf1d0176f7938c8848
abijith pc <abhijithchandrasenan007@gmail.com>
2025-04-16 17:49:36 +0530

    Profile Partial Design

--- lib/features/Auth/presentation/Pages/signuppage.dart
+++ lib/features/Auth/presentation/Pages/signuppage.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:glitchxscndprjt/features/Auth/presentation/Bloc/auth_bloc.dart';
-import 'package:glitchxscndprjt/features/Auth/presentation/Bloc/auth_event.dart';
 import 'package:glitchxscndprjt/features/Auth/presentation/Bloc/auth_state.dart';
 import 'package:glitchxscndprjt/features/Auth/presentation/Pages/verify_emailpage.dart';
 import 'package:glitchxscndprjt/features/Auth/presentation/Widget/screenbackground.dart';
